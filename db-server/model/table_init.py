8189dec3c45616deb2ca68edcf3a25510c6bfe07
小皮皮鹏 <1091375196@qq.com>
2025-01-11 12:54:57 +0800

    简单先写了写

--- db-server/model/table_init.py
+++ db-server/model/table_init.py
@@ -1,5 +1,14 @@
+from peewee import *
 from model import *
 
 
+db = MySQLDatabase("fl", 
+                    "root",
+                    "localhost", 
+                    "password",
+                    charset='utf8mb4')
 def init_table():
     db.create_tables([User])
+
+if __name__ == '__main__':
+    init_table()
